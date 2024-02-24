d93970668b2971bca4972228273a21ec77cf9a1e
M7yassin <mohammd.yassin72@gmail.com>
2024-02-24 22:34:07 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var book= document.getElementById("book")
+book.onclick= function(){alert("Your Appoitment has been booked")}
+var subs= document.getElementById("subs")
+subs.onclick= function(){alert("Congratulations You're now a Subscriber!")}
